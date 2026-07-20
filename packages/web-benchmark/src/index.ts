2b0816f3b98a2c5c3c3d2e7eacedfa1b3c68ec71
Jack <jacklvapple@gmail.com>
2026-07-21 00:32:06 +0800

    feat: export browser benchmark API

---
+++ packages/web-benchmark/src/index.ts
@@ -0,0 +1,12 @@
+export {
+  createBrowserProbeUrl,
+  runBrowserEndpointBenchmark
+} from "./benchmark.ts";
+export type {
+  BrowserBenchmarkDependencies,
+  BrowserBenchmarkOptions,
+  BrowserBenchmarkProgress,
+  BrowserEndpointBenchmarkInput,
+  BrowserEndpointBenchmarkResult,
+  BrowserProbeSample
+} from "./types.ts";
