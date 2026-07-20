99cda1dc6a826729722ed1424c4858981bed4c39
Jack <jacklvapple@gmail.com>
2026-07-21 01:31:15 +0800

    feat: export WebSocket benchmark API

---
+++ packages/websocket-benchmark/src/index.ts
@@ -0,0 +1,15 @@
+export {
+  runWebSocketBenchmark,
+  WebSocketBenchmarkCancelledError
+} from "./benchmark.ts";
+export type {
+  WebSocketBenchmarkDependencies,
+  WebSocketBenchmarkOptions,
+  WebSocketBenchmarkProgress,
+  WebSocketBenchmarkResult,
+  WebSocketBenchmarkStatus,
+  WebSocketCloseInfo,
+  WebSocketCloseLike,
+  WebSocketLike,
+  WebSocketMessageLike
+} from "./types.ts";
